e5f72645a9feda06998b1d954a4865ac8b5acccc
Nicholas <nsellek@protonmail.com>
2023-02-27 19:43:43 -0500

    Revered database.yml changes Fixed issue with `presenter` helper to account for empty relations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,10 @@ class ApplicationController < ActionController::Base
 
   def presenter(obj)
     if obj.is_a? ActiveRecord::Associations::CollectionProxy
-      "#{obj.first.class}Presenter".constantize.from_collection(obj)
+      item = obj.first
+      return obj if item.nil?
+
+      "#{item.class}Presenter".constantize.from_collection(obj)
     else
       "#{obj.class}Presenter".constantize.new(obj)
     end
