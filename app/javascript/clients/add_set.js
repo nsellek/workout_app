e0defc50b0af68c0a7fff57cf8e9b7838eda914e
Nicholas <nsellek@protonmail.com>
2024-01-14 22:26:24 -0500

    Unbound and rebound javascript events on class load fixed #2

--- app/javascript/clients/add_set.js
+++ app/javascript/clients/add_set.js
@@ -6,49 +6,61 @@ export const AddSet = class {
   }
 
   bindModalOpen() {
-    document.addEventListener('turbo:frame-render', event => {
-      let target = event.target
-
-      if (target.id === 'modal') {
-        this.modal = new bootstrap.Modal(target.querySelector('#set-modal'));
-        this.modal.show()
-      }
-    })
+    document.removeEventListener('turbo:frame-render', this.modalOpenEvent)
+    document.addEventListener('turbo:frame-render', this.modalOpenEvent)
   }
 
   bindModalClose() {
-    document.addEventListener('turbo:submit-end', event => {
-      let target = event.target;
-
-      if (target.classList.contains('workout_set_form'))
-        this.modal.hide()
-    });
+    document.removeEventListener('turbo:submit-end', this.modalCloseEvent)
+    document.addEventListener('turbo:submit-end', this.modalCloseEvent);
   }
 
   bindSwipeMenu() {
-    document.addEventListener('click', event => {
-      let target = event.target.closest('.swipe-menu')?.closest('.swap-item'),
-          swapItems = document.querySelectorAll('.swap-item');
-
-      if (target) {
-        if (target.classList.contains('swap-open'))
-          target.classList.remove('swap-open');
-        else {
-          swapItems.forEach( e => e.classList.remove('swap-open'));
-          target.classList.add('swap-open')
-        }
-      }
-    })
+    document.removeEventListener('click', this.swipeMenuEvent)
+    document.addEventListener('click', this.swipeMenuEvent)
   }
 
   bindOffcanvasOpen () {
-    document.addEventListener('turbo:frame-render', event => {
-      let target = event.target
+    document.removeEventListener('turbo:frame-render', this.offcanvasOpenEvent)
+    document.addEventListener('turbo:frame-render', this.offcanvasOpenEvent)
+  }
 
-      if (target.id === 'offcanvas') {
-        new bootstrap.Offcanvas(target.querySelector('#history')).show();
+  modalOpenEvent(event) {
+    let target = event.target
+
+    if (target.id === 'modal') {
+      this.modal = new bootstrap.Modal(target.querySelector('#set-modal'));
+      this.modal.show()
+    }
+  }
+
+  modalCloseEvent(event) {
+    let target = event.target;
+
+    if (target.classList.contains('workout_set_form'))
+      this.modal.hide()
+  }
+
+  swipeMenuEvent(event) {
+    let target = event.target.closest('.swipe-menu')?.closest('.swap-item'),
+        swapItems = document.querySelectorAll('.swap-item');
+
+    if (target) {
+      if (target.classList.contains('swap-open'))
+        target.classList.remove('swap-open');
+      else {
+        swapItems.forEach( e => e.classList.remove('swap-open'));
+        target.classList.add('swap-open')
       }
-    })
+    }
+  }
+
+  offcanvasOpenEvent(event) {
+    let target = event.target
+
+    if (target.id === 'offcanvas') {
+      new bootstrap.Offcanvas(target.querySelector('#history')).show();
+    }
   }
 
   init() {
